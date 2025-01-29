54d87affc3c8a85d8184943c0a8f941bb8d4299a
Shrod Pascual <ethanshrod@gmail.com>
2025-01-29 22:26:29 +0800

    Add files via upload

---
+++ FlappyMain.java
@@ -0,0 +1,27 @@
+package Flappy;
+import javax.swing.*; 
+
+public class FlappyMain {
+
+	public static void main(String[] args) {
+		int uiWidth = 360;
+		int uiHeight = 680;
+		
+		JFrame xiane = new JFrame ("Flappy Shrod");
+		xiane.setVisible(true);
+		xiane.setSize(uiWidth, uiHeight);
+		xiane.setLocationRelativeTo(null);
+		xiane.setResizable(false);
+		xiane.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		GameFile gamefile = new GameFile ();
+		xiane.add(gamefile);
+		xiane.setVisible(true);
+		xiane.pack();
+		gamefile.requestFocus(); //focus 
+		
+		
+		
+	}
+
+}
